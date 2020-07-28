b92d988890b168d5f2dceb9a0a923674f16db0b4
Kasey Arnold <kaseyarnold15@gmail.com>
2020-07-28 01:59:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const output = [];
+    for (let i = 0; i < names.length; i++) {
+        output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return output;
+}
+
+function countDown(start) {
+    for (let i = start; i >= 0; i--) {
+        console.log(i);
+    }
+} 
